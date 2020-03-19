d37487d8cb03f957a2d8ebcd7eed7c4f98a426eb
unknown <sxtnkyl@gmail.com>
2020-03-19 12:41:36 -0400

    Updates

---
+++ precache-manifest.da60a42852f76bcfebe4ebf3b6d97ab7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0e3d0cec752d75edacc478fc5bc0be1",
+    "url": "/portfolio2020/index.html"
+  },
+  {
+    "revision": "b5e9b156368322bf1411",
+    "url": "/portfolio2020/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "fd5bea593b6fb177d293",
+    "url": "/portfolio2020/static/js/2.697aac2e.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/portfolio2020/static/js/2.697aac2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5e9b156368322bf1411",
+    "url": "/portfolio2020/static/js/main.aa44d02c.chunk.js"
+  },
+  {
+    "revision": "c31c45cc238516ab74d0",
+    "url": "/portfolio2020/static/js/runtime-main.e7ba4fc8.js"
+  },
+  {
+    "revision": "60ea64730be05ff93953457393d2cba2",
+    "url": "/portfolio2020/static/media/deepSea3.60ea6473.jpg"
+  },
+  {
+    "revision": "7c6c9a89f0bd6e7f884cbc5379998be6",
+    "url": "/portfolio2020/static/media/espontas.7c6c9a89.jpeg"
+  }
+]);
